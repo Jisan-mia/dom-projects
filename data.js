cf72b79da4ebba4252392d26c817ee53f3586080
Jisan-mia <jisanmia47@gmail.com>
2023-03-24 22:04:49 +0600

    search field reveal project initialized

--- data.js
+++ data.js
@@ -109,6 +109,16 @@ const projectList = [
     difficulty: "Junior",
     tags: ['html', 'css', 'js']
   },
+  {
+    id: 12,
+    name: "Search Field Reveal",
+    description: "I use the scroll event in this project. Basically when you scroll down new content will show up with animation.",
+    image: "https://user-images.githubusercontent.com/61211600/227279125-a59654b2-ce56-4365-bd3e-f115cb8c8dec.png",
+    url: "./projects/12-search-field-reveal/",
+    github: "",
+    difficulty: "Junior",
+    tags: ['html', 'css', 'js']
+  },
 ];
 
 export { projectList };
