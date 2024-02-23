ecfd3d226aceb4c2c06b462959e887fd0ec49641
sadiul-hakim <sadiulhakim@gmail.com>
2024-02-23 20:48:30 +0600

    Color Picker is an DOM application where users can enter the number of color hex code they need. Then they click on the generate button and the application gives them that amount of color code.

---
+++ projects/Color-Picker/app.js
@@ -0,0 +1,45 @@
+const num = document.getElementById("num");
+const generate = document.getElementById("generate");
+const root = document.getElementById("root");
+
+
+generate.onclick = () => {
+    let numberOfColors = Number(num.value);
+
+    const existingHexCodes = new Set();
+
+    for (let i = 0; i < numberOfColors; i++) {  // Generate 10 unique hex codes
+        const newHexCode = generateUniqueHexCode(existingHexCodes);
+        existingHexCodes.add(newHexCode);
+    }
+
+    existingHexCodes.forEach((value) => {
+        let div = generateElement("div",value);
+        root.append(div);
+    })
+
+}
+
+function generateElement(tagName,color){
+    let tag = document.createElement(tagName);
+    tag.style.background = "#"+color;
+    tag.innerText = "#"+color;
+    return tag;
+}
+
+function generateUniqueHexCode(existingHexCodes) {
+    const allChars = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+
+    while (true) {
+        // Shuffle the characters to create randomness
+        allChars.sort(() => Math.random() - 0.5);
+
+        // Take the first 6 characters to form a hex code
+        const hexCode = allChars.slice(0, 6).join('');
+
+        // Check if the generated hex code is unique
+        if (!existingHexCodes.has(hexCode)) {
+            return hexCode;
+        }
+    }
+}
